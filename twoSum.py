ca67fb11791ef91005427084c62b56dc5c101d91
Srihari-vr <sriharivr2003@gmail.com>
2026-02-19 19:05:09 +0530

    Updated twoSum solution

--- twoSum.py
+++ twoSum.py
@@ -1,9 +1,21 @@
+# brute force
+# just traverse the array completly twice to find two pairs 
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mp={} # val : index
-        for i , n in enumerate (nums):
-            diff= target - n
+        for i in range (len(nums)):
+            for j in range (i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return i,j
+                
+# using hashmap 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        mp={} #val : idx
+        for i , n in enumerate(nums):
+            diff = target-n
             if diff in mp:
-                return [mp[diff],i]
-            mp[n]=i
-        return 
+                return [mp[diff],i]:
+            
+            
+
